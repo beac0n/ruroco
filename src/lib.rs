b60bf5ffc6ae26c7d504dbb4892b8d298be2e8c7
beac0n <>
2024-05-19 19:26:18 +0200

    move gen_pem to client.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 pub mod lib {
-    use std::{env, fs};
+    use std::env;
     use std::error::Error;
     use std::path::PathBuf;
     use std::time::SystemTime;
 
-    use log::debug;
-    use openssl::rsa::Rsa;
-
     pub const SOCKET_DIR: &str = "/tmp/ruroco/";
     pub const SOCKET_FILE_PATH: &str = "/tmp/ruroco/ruroco.socket";
+    pub const PEM_DIR_ERR_PREFIX: &str = "Could not generate public and private PEM files:";
 
     pub fn init_logger() {
         env_logger::builder()
@@ -22,40 +20,10 @@ pub mod lib {
             .as_secs())
     }
 
-    pub fn gen_pem() -> Result<(), Box<dyn Error>> {
-        let key_size = 8192;
-
-        return match (get_default_pem_private(), get_default_pem_public()) {
-            (private, public) if private.exists() || public.exists() => {
-                let msg = format!(
-                "Could not generate new rsa key with {key_size} bits, because {private:?} or {public:?} already exists"
-            );
-                Err(msg.into())
-            }
-            (private, public) => {
-                debug!(
-                "Generating new rsa key with {key_size} bits and saving it to {private:?} and {public:?}. This might take a while...",
-            );
-                let rsa = Rsa::generate(key_size)?;
-                fs::write(&private, rsa.private_key_to_pem()?)?;
-                fs::write(&public, rsa.public_key_to_pem()?)?;
-                Ok(())
-            }
-        };
-    }
-
-    pub fn get_default_pem_public() -> PathBuf {
-        get_default_pem_path("ruroco_public.pem")
-    }
-
-    pub fn get_default_pem_private() -> PathBuf {
-        get_default_pem_path("ruroco_private.pem")
-    }
-
-    fn get_default_pem_path(pem_name: &str) -> PathBuf {
+    pub fn get_path(file_name: &str) -> PathBuf {
         return match env::current_dir() {
-            Ok(dir) => dir.join(pem_name),
-            Err(_) => PathBuf::from(pem_name),
+            Ok(dir) => dir.join(file_name),
+            Err(_) => PathBuf::from(file_name),
         };
     }
 }
