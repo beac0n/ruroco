aa363d933206f6bca96e7c49db6e4b7de5c8ffef
beac0n <>
2024-05-19 18:33:59 +0200

    integrate server and commander and add message to determine which command to execute

--- src/bin/commander.rs
+++ src/bin/commander.rs
@@ -1,12 +1,16 @@
 use std::{fs, str, thread};
 use std::error::Error;
 use std::fs::Permissions;
+use std::io::prelude::*;
 use std::os::unix::fs::PermissionsExt;
-use std::os::unix::net::UnixListener;
+use std::os::unix::net::{UnixListener, UnixStream};
 use std::process::Command;
 use std::time::Duration;
 
 use clap::Parser;
+use log::{error, info, warn};
+
+use ruroco::lib::{init_logger, SOCKET_DIR, SOCKET_FILE_PATH};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -20,40 +24,54 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    init_logger();
     let args = Cli::parse();
-    let start_command = args.start;
-    let stop_command = args.stop;
-    let sleep_time = args.sleep;
 
-    let socket_dir = "/tmp/ruroco/";
-    println!("Creating ruroco socket dir {socket_dir}");
-    fs::create_dir_all(socket_dir)?;
+    info!("Creating ruroco socket dir {}", SOCKET_DIR);
+    fs::create_dir_all(SOCKET_DIR)?;
 
-    let socket_file_path = socket_dir.to_owned() + "ruroco.socket";
-    println!("Removing already existing socket file {socket_file_path}");
-    let _ = fs::remove_file(&socket_file_path);
+    info!("Removing already existing socket file {}", SOCKET_FILE_PATH);
+    let _ = fs::remove_file(SOCKET_FILE_PATH);
 
     let mode = 0o600;
-    println!("Listing Unix Listener on {socket_file_path} with permissions {mode:o}");
-    let listener = UnixListener::bind(&socket_file_path)?;
-    fs::set_permissions(&socket_file_path, Permissions::from_mode(mode))?;
+    info!("Listing Unix Listener on {SOCKET_FILE_PATH} with permissions {mode:o}");
+    let listener = UnixListener::bind(SOCKET_FILE_PATH)?;
+    fs::set_permissions(SOCKET_FILE_PATH, Permissions::from_mode(mode))?;
     for stream in listener.incoming() {
         match stream {
-            Ok(_) => {
-                println!("Starting cycle");
-                run_command(&start_command);
-                println!("Sleeping for {sleep_time} seconds");
-                thread::sleep(Duration::from_secs(sleep_time));
-                run_command(&stop_command);
-                println!("Finished cycle");
-            }
-            Err(err) => {
-                println!("Connection failed: {err}");
+            Ok(mut stream) => match get_command_msg(&mut stream) {
+                Ok(msg) if msg == "default" => {
+                    run_command_cycle(&args.start, &args.stop, args.sleep);
+                }
+                Ok(msg) => {
+                    warn!("Unknown command message {msg}");
+                }
+                Err(e) => {
+                    error!("Failed to read command message: {e}")
+                }
+            },
+            Err(e) => {
+                error!("Connection failed: {e}");
             }
         }
     }
-    let _ = fs::remove_file(&socket_file_path);
+    let _ = fs::remove_file(SOCKET_FILE_PATH);
     Ok(())
+}
+
+fn get_command_msg(stream: &mut UnixStream) -> Result<String, Box<dyn Error>> {
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer)?;
+    return Ok(buffer);
+}
+
+fn run_command_cycle(start: &str, stop: &str, sleep: u64) {
+    info!("Starting cycle");
+    run_command(start);
+    info!("Sleeping for {sleep} seconds");
+    thread::sleep(Duration::from_secs(sleep));
+    run_command(stop);
+    info!("Finished cycle");
 }
 
 fn run_command(command: &str) {
@@ -65,10 +83,10 @@ fn run_command(command: &str) {
         Ok(result) => {
             let stdout = str::from_utf8(&result.stdout).unwrap_or("");
             let stderr = str::from_utf8(&result.stderr).unwrap_or("");
-            println!("Successfully executed {command}\nstdout: {stdout}\nstderr: {stderr}")
+            info!("Successfully executed {command}\nstdout: {stdout}\nstderr: {stderr}")
         }
-        Err(err) => {
-            println!("Error executing {command} - {err}")
+        Err(e) => {
+            error!("Error executing {command} - {e}")
         }
     };
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,35 +1,38 @@
-use std::error::Error;
-use std::net::UdpSocket;
-use std::path::PathBuf;
 use std::{fs, str};
+use std::error::Error;
+use std::io::prelude::*;
+use std::net::UdpSocket;
+use std::os::unix::net::UnixStream;
+use std::path::PathBuf;
 
 use clap::Parser;
 use log::{error, info};
 use openssl::pkey::Public;
 use openssl::rsa::{Padding, Rsa};
 
-use ruroco::lib;
+use ruroco::lib::{get_default_pem_public, init_logger, SOCKET_FILE_PATH, time};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(short, long, default_value_t = String::from("127.0.0.1:8080"))]
     address: String,
-    #[arg(short, long, default_value = lib::get_default_pem_public().into_os_string())]
+    #[arg(short, long, default_value = get_default_pem_public().into_os_string())]
     pem_path: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    lib::init_logger();
+    init_logger();
     let args = Cli::parse();
+    let address = &args.address;
 
     info!("Starting server on udp://{address}, loading PEM from {} ...", args.pem_path.display());
     let pem_data = fs::read(args.pem_path)?;
     let rsa: Rsa<Public> = Rsa::public_key_from_pem(&pem_data)?;
-    let socket = UdpSocket::bind(&args.address)?;
+    let socket = UdpSocket::bind(address)?;
 
     loop {
-        iteration(&rsa, &args.address, &socket);
+        iteration(&rsa, address, &socket);
     }
 }
 
@@ -42,7 +45,7 @@ fn iteration(rsa: &Rsa<Public>, address: &str, socket: &UdpSocket) {
             error!("Invalid read count {read_count}, expected {expected_read_count} - from {src}")
         }
         Ok(_) => validate_encrypted(rsa, &encrypted_data),
-        Err(_) => error!("Could not recv_from socket from udp://{address}"),
+        Err(e) => error!("Could not recv_from socket from udp://{address}: {e}"),
     };
 }
 
@@ -57,17 +60,32 @@ fn validate_encrypted(rsa: &Rsa<Public>, encrypted_data: &[u8; 1024]) {
 fn validate_decrypted(decrypted_data: &mut Vec<u8>, count: usize) {
     decrypted_data.truncate(count);
     let timestamp = vec_u8_to_u64(&decrypted_data);
-    return match lib::time() {
+    return match time() {
         Ok(now) if timestamp > now => error!("Invalid content {timestamp} is newer than now {now}"),
         Ok(now) if timestamp < now - 5 => {
             error!("Invalid content {timestamp} is older than now {now} - 5 = {}", now - 5)
         }
         Ok(_) => {
-            // TODO: execute command executor
-            info!("Successfully validated data - {timestamp} is not too old/new")
+            info!("Successfully validated data - {timestamp} is not too old/new");
+            match write_to_socket() {
+                Ok(_) => {
+                    info!("Successfully sent data to commander");
+                }
+                Err(e) => {
+                    error!("Could not send data to commander: {e}")
+                }
+            }
         }
         Err(e) => error!("Could not get current time: {e}"),
     };
+}
+
+fn write_to_socket() -> Result<(), Box<dyn Error>> {
+    let mut stream = UnixStream::connect(SOCKET_FILE_PATH)?;
+    let command_name = "default"; // TODO: allow for different command names -> has to come from client
+    stream.write_all(command_name.as_bytes())?;
+    stream.flush()?;
+    Ok(())
 }
 
 fn vec_u8_to_u64(data: &Vec<u8>) -> u64 {
