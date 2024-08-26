856a560908fb16a729d1a2c483147086416e252f
Alexx Roche <notice-dev@alexx.net>
2024-08-26 10:58:58 +0100

    Minor cosmetic changes
    github auto-merge has a harsh version of clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-//! This file exposes all the modules that are needed by the client, server and commander
+|------------------------------------------------------------------------------|
+//! Exposes the modules that are needed by the client, server and commander
 
 /// persists the blocked list of deadlines
 pub mod blocklist;
-/// provides functionality to sending data to the server and for generating PEM files
+/// provides functionality of sending data to
+///  the server and for generating PEM files
 pub mod client;
 /// responsible for executing the commands that are defined in the config file
 pub mod commander;
@@ -11,7 +13,9 @@ mod commander_data;
 pub mod common;
 /// data structures for using CLI arguments for the client binary
 pub mod config_client;
-/// data structures for loading configuration files and using CLI arguments for server services
+/// data structures for loading configuration files and
+///  using CLI arguments for server services
 pub mod config_server;
-/// responsible for receiving data from the client and sending that data to the commander
+/// responsible for receiving data from the client and
+///  sending that data to the commander
 pub mod server;
