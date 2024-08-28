02361f779d99aa29b8bc7799d2bf7a433a3746e4
Alexx Roche <notice-dev@alexx.net>
2024-08-28 22:57:48 +0100

    merging in beac0n's changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//! Exposes the modules that are needed by the client, server and commander
+#![allow(clippy::too_long_first_doc_paragraph)]
+//! This file exposes all the modules that are needed by the client, server and commander
 
 /// persists the blocked list of deadlines
 pub mod blocklist;
-/// provides functionality of sending data to
-///  the server and for generating PEM files
+/// provides functionality to sending data to the server and for generating PEM files
 pub mod client;
 /// responsible for executing the commands that are defined in the config file
 pub mod commander;
@@ -12,9 +12,7 @@ mod commander_data;
 pub mod common;
 /// data structures for using CLI arguments for the client binary
 pub mod config_client;
-/// data structures for loading configuration files and
-///  using CLI arguments for server services
+/// data structures for loading configuration files and using CLI arguments for server services
 pub mod config_server;
-/// responsible for receiving data from the client and
-///  sending that data to the commander
+/// responsible for receiving data from the client and sending that data to the commander
 pub mod server;
