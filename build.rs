c18639089acf4433aa9be24a3c7364f7e4dc7e6e
beac0n <gh@schempp.dev>
2024-11-02 21:13:42 +0100

    use fluent as style for ui

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let config = slint_build::CompilerConfiguration::new().with_style("native".into());
+    let config = slint_build::CompilerConfiguration::new().with_style("fluent".into());
 
     slint_build::compile_with_config("src/ui/app-window.slint", config)
         .expect("Slint build failed");
