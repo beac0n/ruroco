f85e602c8ad278066f887616e65c16e6621f1735
beac0n <gh@schempp.dev>
2024-12-26 00:32:55 +0100

    make sure commands list is only every read/writeable by one thread

--- src/ui.rs
+++ src/ui.rs
@@ -8,11 +8,12 @@ use crate::saved_command_list::CommandsList;
 use crate::slint_bridge;
 use crate::slint_bridge::CommandTuple;
 use clap::Parser;
-use slint::{ComponentHandle, Model, ModelRc, SharedString, VecModel};
+use slint::{ComponentHandle, Model, ModelRc, SharedString, VecModel, Weak};
 use slint_bridge::{App, CommandLogic};
 use std::error::Error;
 use std::fs;
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 pub fn run_ui() -> Result<(), Box<dyn Error>> {
     let app = App::new()?;
@@ -41,36 +42,33 @@ pub fn run_ui() -> Result<(), Box<dyn Error>> {
     globals.set_deadline(DEFAULT_DEADLINE.to_string().into());
     globals.set_ntp(SharedString::from(NTP_SYSTEM));
 
+    let cmds_list_arc_mutex = Arc::new(Mutex::new(CommandsList::create(&get_conf_dir())));
+
     globals.on_add_command({
         let app_handle = app.as_weak();
-        let mut persistent_commands_list = CommandsList::create(&get_conf_dir());
+        let cmds_list_mutex = Arc::clone(&cmds_list_arc_mutex);
         move |cmd| {
-            let binding = app_handle.unwrap();
-            let commands_list_rc = binding.global::<CommandLogic>().get_commands_list();
-            let commands_list: &VecModel<CommandTuple> = commands_list_rc
-                .as_any()
-                .downcast_ref()
-                .expect("Expected an initialized commands_list, found None");
-
             info(&format!("Adding new command: {cmd}"));
+            let mut persistent_commands_list = cmds_list_mutex.lock().unwrap();
             persistent_commands_list.add(cmd.clone());
+
+            let commands_list_rc = get_commands_list_rc(&app_handle);
+            let commands_list = get_commands_list(&commands_list_rc);
             commands_list.push(CommandsList::create_command_tuple(cmd));
         }
     });
 
     globals.on_del_command({
         let app_handle = app.as_weak();
-        let mut persistent_commands_list = CommandsList::create(&get_conf_dir());
-        move |cmd| {
-            let binding = app_handle.unwrap();
-            let commands_list_rc = binding.global::<CommandLogic>().get_commands_list();
-            let commands_list: &VecModel<CommandTuple> = commands_list_rc
-                .as_any()
-                .downcast_ref()
-                .expect("Expected an initialized commands_list, found None");
+        let cmds_list_mutex = Arc::clone(&cmds_list_arc_mutex);
 
+        move |cmd| {
             info(&format!("Removing command: {cmd}"));
+            let mut persistent_commands_list = cmds_list_mutex.lock().unwrap();
             persistent_commands_list.remove(cmd.clone());
+
+            let commands_list_rc = get_commands_list_rc(&app_handle);
+            let commands_list = get_commands_list(&commands_list_rc);
             commands_list
                 .iter()
                 .enumerate()
@@ -101,4 +99,15 @@ pub fn run_ui() -> Result<(), Box<dyn Error>> {
     app.run()?;
 
     Ok(())
+}
+
+fn get_commands_list_rc(app_handle: &Weak<App>) -> ModelRc<CommandTuple> {
+    app_handle.unwrap().global::<CommandLogic>().get_commands_list()
+}
+
+fn get_commands_list(commands_list_rc: &ModelRc<CommandTuple>) -> &VecModel<CommandTuple> {
+    commands_list_rc
+        .as_any()
+        .downcast_ref()
+        .expect("Expected an initialized commands_list, found None")
 }
