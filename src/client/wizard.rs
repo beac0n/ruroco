5fa8c3bcb61ac044246313ba41958d882f080762
beac0n <>
2025-04-20 20:30:49 +0200

    add logs

--- src/client/wizard.rs
+++ src/client/wizard.rs
@@ -26,7 +26,7 @@ impl Wizard {
 
     pub fn run(&self) -> Result<(), String> {
         Self::create_ruroco_user()?;
-        Updater::create(true, None, None, true)?.update()?;
+        Self::update()?;
 
         Self::write_data(RUROCO_SERVICE_FILE_PATH, RUROCO_SERVICE_FILE_DATA)?;
         Self::write_data(COMMANDER_SERVICE_FILE_PATH, COMMANDER_SERVICE_FILE_DATA)?;
@@ -37,21 +37,29 @@ impl Wizard {
         Self::enable_systemd_services()?;
         Self::start_systemd_services()?;
 
-        info("### Ruroco Server Side installed successfully");
-        info(&format!("# Please check the config in {CONFIG_TOML_PATH} and edit it accordingly"));
-        info("# Generate public and private pem files with ruroco-client gen command");
+        info("=========================================");
+        info("Ruroco Server Side installed successfully");
+        info(&format!("Please check the config in {CONFIG_TOML_PATH} and edit it accordingly"));
+        info("Generate public and private pem files with ruroco-client gen command");
         info(&format!(
-            "# Save public pem file you just generated in 'config_dir' as defined in {CONFIG_TOML_PATH}"
+            "Save public pem file you just generated in 'config_dir' as defined in {CONFIG_TOML_PATH}"
         ));
         info(&format!(
-            "# Save public and private pem file you just generated on your client in {:?}",
+            "Save public and private pem file you just generated on your client in {:?}",
             get_conf_dir()
         ));
+        info("=========================================");
 
         Ok(())
     }
 
+    fn update() -> Result<(), String> {
+        info("Updating/Installing ruroco server binaries");
+        Updater::create(true, None, None, true)?.update()
+    }
+
     fn init_config_file() -> Result<(), String> {
+        info(&format!("Initializing config file {CONFIG_TOML_PATH}"));
         if !Path::new(CONFIG_TOML_PATH).exists() {
             Self::write_data(CONFIG_TOML_PATH, CONFIG_TOML_FILE_DATA)?;
         }
@@ -61,6 +69,7 @@ impl Wizard {
     }
 
     fn start_systemd_services() -> Result<(), String> {
+        info("Starting systemd services ...");
         Command::new("systemctl")
             .arg("start")
             .arg("ruroco.service")
@@ -72,6 +81,7 @@ impl Wizard {
     }
 
     fn enable_systemd_services() -> Result<(), String> {
+        info("Enabling systemd services ...");
         Command::new("systemctl")
             .arg("enable")
             .arg("ruroco.service")
@@ -83,6 +93,7 @@ impl Wizard {
     }
 
     fn reload_systemd_daemon() -> Result<(), String> {
+        info("Reloading systemd daemon ...");
         Command::new("systemctl")
             .arg("daemon-reload")
             .status()
@@ -91,6 +102,7 @@ impl Wizard {
     }
 
     fn create_ruroco_user() -> Result<(), String> {
+        info("Creating user 'ruroco' ...");
         Command::new("useradd")
             .arg("--system")
             .arg("ruroco")
@@ -102,6 +114,7 @@ impl Wizard {
     }
 
     fn write_data(path: &str, data: &[u8]) -> Result<(), String> {
+        info(&format!("Creating {path} ..."));
         let mut file =
             fs::File::create(path).map_err(|e| format!("Failed to create {path}: {e}"))?;
         file.write_all(data).map_err(|e| format!("Failed to write to {path}: {e}"))?;
