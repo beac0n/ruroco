994f578e6f1565f8159c687a54fe1f73c8390d09
beac0n <gh@schempp.dev>
2025-05-03 12:47:19 +0200

    refactor

--- android.nix
+++ android.nix
@@ -1,24 +1,31 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
+let
+  root = toString ./.;
+  androidHome="${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
+  arch=pkgs.stdenv.hostPlatform.parsed.cpu.name;
+in pkgs.mkShell {
   buildInputs = with pkgs; [
-    androidenv.androidPkgs.androidsdk
-    rustup
+    rustup  # xbuild needs rustup
+    androidenv.androidPkgs.androidsdk  # for building android
+    jdk23_headless # for building android
     openssl
-    curl
-    jdk17
-    perl
+    perl  # building openSSL requires perl
+    curl  # needed for downloading skia libraries
   ];
 
-  ANDROID_HOME="${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
-  ANDROID_NDK = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk/ndk-bundle";
-  OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include/openssl"
-  OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
-  OPENSSL_ROOT_DIR="${pkgs.openssl.out}"
+  ANDROID_HOME="${androidHome}";
+  ANDROID_NDK_ROOT="${androidHome}/ndk-bundle";
+
+  CARGO_HOME="${root}/.nix-cargo";
+
+  OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include/openssl";
+  OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib";
+  OPENSSL_ROOT_DIR="${pkgs.openssl.out}";
 
   shellHook = ''
     rustup default stable
-    export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
+    export PATH=$CARGO_HOME/bin:$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-${arch}/bin/:$PATH
   '';
 
 }
