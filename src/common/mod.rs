c8f0c687610e1cfa3cc744795a3f5acf8a873470
beac0n <gh@schempp.dev>
2025-05-04 12:13:59 +0200

    update tests

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -202,16 +202,21 @@ mod tests {
 
     #[test]
     fn test_time_from_ntp_server() {
-        let start = time().unwrap();
         let first_time = time_from_ntp("europe.pool.ntp.org:123").unwrap();
         let second_time = time_from_ntp("0.europe.pool.ntp.org:123").unwrap();
-        let diff = second_time - first_time;
 
-        assert!(diff > 0);
-        let one_second = 1000000000;
-        assert!(diff < one_second);
-        assert!(first_time > start);
-        assert!(second_time > start);
+        let diff = if second_time > first_time {
+            second_time - first_time
+        } else {
+            first_time - second_time
+        };
+
+        dbg!(first_time);
+        dbg!(second_time);
+        dbg!(diff);
+
+        let max_allowed_diff = 500_000_000; // 0.5 seconds
+        assert!(diff < max_allowed_diff, "Time difference too large: {diff}");
     }
 
     #[test]
