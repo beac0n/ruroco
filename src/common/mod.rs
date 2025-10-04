76548b7f63f58810cea7fdfef9c0bfae135a0ad7
beac0n <gh@schempp.dev>
2025-10-04 12:55:42 +0200

    format

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -131,7 +131,7 @@ const SECONDS_PER_DAY: u64 = 86400;
 const MONTHS: [u64; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 fn is_leap_year(year: u64) -> bool {
-    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    (year.is_multiple_of(4) && !year.is_multiple_of(100)) || year.is_multiple_of(400)
 }
 
 fn days_in_month(month: u64, year: u64) -> u64 {
