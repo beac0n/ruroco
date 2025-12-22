417898542a6cf9a81d587bcd8c79632e1d14bade
beac0n <gh@schempp.dev>
2025-12-22 16:35:44 +0100

    refactor

--- src/common/protocol/serialization.rs
+++ src/common/protocol/serialization.rs
@@ -1,13 +1,15 @@
 use std::net::{IpAddr, Ipv6Addr};
 
-pub(crate) fn serialize_ip(ip: &IpAddr) -> [u8; 16] {
+const IP_SIZE: usize = 16;
+
+pub(crate) fn serialize_ip(ip: &IpAddr) -> [u8; IP_SIZE] {
     match ip {
         IpAddr::V4(v4) => v4.to_ipv6_mapped().octets(),
         IpAddr::V6(v6) => v6.octets(),
     }
 }
 
-pub(crate) fn deserialize_ip(data: [u8; 16]) -> IpAddr {
+pub(crate) fn deserialize_ip(data: [u8; IP_SIZE]) -> IpAddr {
     let v6 = Ipv6Addr::from(data);
     if let Some(v4) = v6.to_ipv4_mapped() {
         IpAddr::V4(v4)
