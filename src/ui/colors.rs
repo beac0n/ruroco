efeda610e6a33c2514fe6096fe2c2321a2503d9c
beac0n <gh@schempp.dev>
2026-05-18 22:08:32 +0200

    refactor

--- src/ui/colors.rs
+++ src/ui/colors.rs
@@ -1,17 +1,6 @@
 use eframe::egui::Color32;
 
+pub(crate) const BLUE: Color32 = Color32::from_rgb(25, 118, 210);
 pub(crate) const GREEN: Color32 = Color32::from_rgb(56, 142, 60);
 pub(crate) const RED: Color32 = Color32::from_rgb(211, 47, 47);
 pub(crate) const GRAY: Color32 = Color32::from_rgb(204, 204, 204);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_color_constants() {
-        assert_eq!(GREEN, Color32::from_rgb(56, 142, 60));
-        assert_eq!(RED, Color32::from_rgb(211, 47, 47));
-        assert_eq!(GRAY, Color32::from_rgb(204, 204, 204));
-    }
-}
